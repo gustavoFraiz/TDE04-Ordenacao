8b830b82c3ef6bbcc60a60124415e5daa9dc537f
Gustavo Fraiz <gugafraiz7@gmail.com>
2023-11-04 15:42:56 -0300

    Tudo funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int arr[] = {21,141,12,14,5,123,44,5,521,24,1};
@@ -6,8 +7,34 @@ public class Main {
         MergeSort merge = new MergeSort();
         QuickSort quick = new QuickSort();
 
-        quick.quickSort(arr, 0, arr.length - 1);
-        quick.print(arr, arr.length);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 - Bubble, 2 - Selection, 3 - Merge, 4 - QuickSort");
+        int start = scanner.nextInt();
+        System.out.println("---------------------");
+
+        switch(start){
+
+            case 1:
+                System.out.println("Bubble Sort");
+                bubble.sort(arr);
+                bubble.print(arr);
+                break;
+            case 2:
+                System.out.println("Selection Sort");
+                selection.sort(arr);
+                selection.print(arr);
+                break;
+            case 3:
+                System.out.println("Merge Sort");
+                merge.mergeSort(arr, arr.length);
+                merge.print(arr, arr.length);
+                break;
+            case 4:
+                System.out.println("Quick Sort");
+                quick.quickSort(arr, 0, arr.length - 1);
+                quick.print(arr, arr.length);
+                break;
+        }
 
     }
 }
